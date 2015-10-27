cfc0667bd8b7fff13ba415a0f3e0489db7aea6c8
Steve Danner <steven.danner@gmail.com>
2015-10-26 20:59:45 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+function normalizePort(val) {
+		var port = parseInt(val, 10);
+
+		if (isNaN(port)) {
+			// named pipe
+			return val;
+		}
+
+		if (port >= 0) {
+			// port number
+			return port;
+		}
+
+		return false;
+}
+
+app.set('port', normalizePort(process.env.PORT || '3000'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
