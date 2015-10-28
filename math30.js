2cd1e303f53d2b28ed90531dba8b7ec0da7fda99
Steve Danner <steven.danner@gmail.com>
2015-10-27 21:44:20 -0500

    2015-10-27

--- math30.js
+++ math30.js
@@ -1,3 +1,5 @@
+var $ = window.$;
+
 $(document).ready(function () {
 	window.viewModel.init();
 });
@@ -11,9 +13,17 @@ var Math30ViewModel = function () {
 	self.selectSubtractionButton = null;
 	self.submitOptionsButton = null;
 	self.gameContainer = null;
+	self.currentProblem = null;
+	self.currentProblemIndex = 0;
+	self.problems = [];
+	self.problemCount = 30;
+	//
+	//TODO: build timer functionality and UI
+	//TODO: build in ability to have 
+	//
 	
 	self.init = function () {
-		//set vars
+		//set UI vars
 		self.optionsContainer = $("#optionsContainer");
 		self.typeButtons = $(".type-button");
 		self.selectAdditionButton = $("#btnAddition");
@@ -36,11 +46,92 @@ var Math30ViewModel = function () {
 		}
 	};
 	
+	self.buildProblems = function () {
+		for (var i = 0; i < self.problemCount; i++) {
+			self.problems.push(new Problem(i + 1));
+		}
+		self.currentProblemIndex = 0;
+	};
+	
+	self.nextQuestion = function () {
+		if (self.currentProblemIndex + 1 < self.problemCount) {
+			self.currentProblemIndex++;
+		}
+		self.problems[self.currentProblemIndex].paint();
+	};	
+	
 	self.submitOptions = function () {
-		//TODO: validation
+		//
+		//TODO: validation of options\
+		//
 		self.optionsContainer.hide();
+		
+		self.buildProblems();
+		self.problems[self.currentProblemIndex].paint();
 		self.gameContainer.show();
 	};
+};
+
+var Problem = function (index) {
+	var self = this;
+	
+	self.init();
+	self.index = index;
+	self.numerator = Problem.random();
+	self.denominator = Problem.random();
+	self.operator = Problem.randomOperator();
+	self.answer = '';
+	
+	//make sure numerator is >= denominator
+	if (self.numerator < self.denominator) {
+		self.denominator = [self.numerator, self.numerator = self.denominator][0];
+	}
+	
+	//
+	//TODO: calculate correct answer
+	//
+	
+	self.paint = function () {
+		Problem.problemNumberContainer.text(self.index + ".");
+		Problem.numeratorContainer.text(self.numerator);
+		Problem.denominatorContainer.text(self.denominator);
+		Problem.operatorContainer.text(self.operator);
+		Problem.answerInput.val(self.answer);
+		Problem.answerInput.focus();
+	};
+};
+
+Problem.operators = ['+', '-'];
+Problem.min = 0;
+Problem.max = 10;
+Problem.isInitialized = false;
+
+Problem.prototype.init = function () {
+	if (!Problem.isInitialized) {
+		Problem.problemNumberContainer = $("#problemNumberContainer");
+		Problem.numeratorContainer = $("#numeratorContainer");
+		Problem.denominatorContainer = $("#denominatorContainer");
+		Problem.operatorContainer = $("#operatorContainer");
+		Problem.answerContainer = $("#answerContainer");
+		Problem.answerInput = $("#answerInput");
+		Problem.answerInput.keypress(Problem.onKeyPress);
+		Problem.isInitialized = true;
+	}
+};
+Problem.onKeyPress = function (evt) {
+	if (evt.keyCode === 13) {
+		window.viewModel.nextQuestion();
+	}	
+};
+Problem.random = function (min, max) {
+	if (typeof min === 'undefined') min = Problem.min;
+	if (typeof max === 'undefined') max = Problem.max;
+	
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+Problem.randomOperator = function () {
+	var idx = Problem.random(0, Problem.operators.length - 1);	
+	return Problem.operators[idx];
 };
 
 window.viewModel = new Math30ViewModel();
